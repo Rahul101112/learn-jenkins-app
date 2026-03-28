02b42d9e666886ffc06e22d9a58152ee6a393117
rahul101112 <mr.rahul1011@gmail.com>
2026-03-28 06:47:15 +0530

    Final Deployment completed

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Implementation: of Docker and Jenkins on the Ubuntu server completed and learning from Udemy course.
-        </a>
-      </header>
-      <p>
-          Application version: 5
+    <div style={styles.app}>
+      <style>
+        {`
+          @keyframes spin {
+            from { transform: rotate(0deg); }
+            to { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        style={styles.logo}
+        alt="logo"
+      />
+
+      <a
+        href="https://example.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={styles.link}
+      >
+        Implementation: of Docker and Jenkins on the Ubuntu server completed and learning from Udemy course.
+      </a>
+
+      <p style={styles.version}>
+        Application version: 95
       </p>
     </div>
   );
